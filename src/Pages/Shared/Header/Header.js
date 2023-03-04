5bd2ac4ff598b5a257bb6111957c82f5dcca59e4
Rakibul-98 <rakibul.rupom2001@gmail.com>
2023-03-04 20:36:31 +0600

    folder structure and initial setup completed

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import icon from '../../../assets/icon/default.png';
+
+const menuItems= <>
+    <li><Link>Item 1</Link></li>
+    <li><Link>Item 2</Link></li>
+    <li><Link>Item 3</Link></li>
+</>
+
+const Header = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to='/'>
+                    <img className='h-16' src={icon} alt="" />
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {menuItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Get started</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
